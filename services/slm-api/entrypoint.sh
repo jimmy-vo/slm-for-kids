60c4be8b6b3954d299e9c7e3c5483d5c74346f22
Jimmy Vo <jimmy.vo.m5@gmail.com>
2025-04-27 20:06:54 -0500

    Update

---
+++ services/slm-api/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Starting Ollama server..."
+ollama serve &
+SERVE_PID=$!
+
+echo "Waiting for Ollama server to be active..."
+while ! ollama list | grep -q 'NAME'; do
+  sleep 1
+done
+
+ollama pull $SLM_MODEL
+
+
+wait $SERVE_PID
